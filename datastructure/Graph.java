8d928af1c977fc9c164fb22c0a0c4bebacfda9af
Cuesta0113 <78290201+sushi3085@users.noreply.github.com>
2021-11-24 13:36:59 +0800

    歸類資料

---
+++ datastructure/Graph.java
@@ -0,0 +1,21 @@
+public class Graph{
+	public static void main(String[] args){
+		Node[] nodes = new Node[5];
+		for(int i=0; i<nodes.length; i++){
+			nodes[i] = new Node(i*2);
+		}
+		
+	}
+}
+
+class Node{// this is a weighted node
+	int val;
+	List<int[]> neightbors;
+
+	public Node(int x){
+		this.val = x;
+	}
+	public Node(int x, List<int[]> nodes){
+		this.neightbors = nodes;
+	}
+}
