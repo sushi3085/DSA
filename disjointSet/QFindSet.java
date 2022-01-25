0b9adadac8a6284b56cd3e95807beb384be1a316
Cuesta0113 <78290201+sushi3085@users.noreply.github.com>
2022-01-25 10:10:41 +0800

    complete QFindSet.java

---
+++ disjointSet/QFindSet.java
@@ -0,0 +1,22 @@
+public class QFindSet{
+	public QFindSet(int size){
+		this.datas = new int[size];
+		for(int i=0; i<size; i++)
+			datas[i] = i;
+	}
+
+	public int find(int node){
+		return datas[node];
+	}
+
+	public void connect(int a, int b){
+		int rootX = find(a)
+		int rootY = find(b)
+		if(rootX!=rootY){
+			for(int i=0; i<datas.length; i++)
+				if(datas[i]==rootY) datas[i] = rootX;
+		}
+	}
+	
+	private void int[] datas;
+}
