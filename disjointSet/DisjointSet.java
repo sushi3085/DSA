bfcbe2f509d7af273e87d10a54391ae9e947939e
Cuesta0113 <78290201+sushi3085@users.noreply.github.com>
2022-01-25 10:47:45 +0800

    DJS complete

---
+++ disjointSet/DisjointSet.java
@@ -0,0 +1,34 @@
+public class DisJointSet{
+	public DisJointSet(int size){
+		this.datas = new int[size];
+		this.rank = new int[size];
+		for(int i=0; i<size; i++)
+			datas[i] = rank[i] = i;
+	}
+
+	public int find(int node){
+		while(node!=datas[node])
+			node = datas[node]
+		return node
+	}
+
+	public void connect(int a, int b){
+		int rootX = find(a);
+		int rootY = find(b);
+		if(rootX != rootY){
+			if(rank[rootX] > rank[rootY]){
+				datas[rootY] = rootX;
+			}
+			else if(rank[rootX] < rank[rootY]){
+				datas[rootX] = rootY;
+			}
+			else{
+				rank[rootX]++;
+				datas[rootY] = rootX;
+			}
+		}
+	}
+
+	private int[] datas;
+	private int[] rank;
+}
